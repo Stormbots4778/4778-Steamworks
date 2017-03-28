f4253d7ac0f46a0f1a956788fb7e01ca0e283dcd
Stormbots <Stormbots>
2017-03-28 15:53:17 -0500

    Ima kill myself

--- src/org/usfirst/frc/team4778/robot/OI.java
+++ src/org/usfirst/frc/team4778/robot/OI.java
@@ -13,40 +13,24 @@ public class OI {
 	// Joysticks
 	public static Joystick leftJoystick = new Joystick(1);
 	public static Joystick rightJoystick = new Joystick(0);
-	// Redundant Gamepad
+	// Logitech Gamepad
+	public static Joystick gamepad = new Joystick(2);
 	// Button Board 
 	//
 	// Update when new drivers station is built
 	//
-	/*
 	// Hanging Mechanism
-	public static JoystickButton coil = new JoystickButton(nip, value);
-	public static JoystickButton uncoil = new JoystickButton(nip, value);
+	public static JoystickButton coilG = new JoystickButton(gamepad, 8);
+	public static JoystickButton uncoilG = new JoystickButton(gamepad, 7);
 	// Intake
-	public static JoystickButton intake = new JoystickButton(nip, value);
-	public static JoystickButton outtake = new JoystickButton(nip, value);
+	public static JoystickButton intakeG = new JoystickButton(gamepad, 6);
+	public static JoystickButton outtakeG = new JoystickButton(gamepad, 5);
 	// Shooter
-	public static JoystickButton shoot = new JoystickButton(nip, value);
+	public static JoystickButton shootG = new JoystickButton(gamepad, 2);
 	// Hopper
-	public static JoystickButton dispense = new JoystickButton(nip, value);
+	public static JoystickButton dispenseG = new JoystickButton(gamepad, 3);
 	// Ball Feed
-	public static JoystickButton feedG = new JoystickButton(nip, value);
-	*/
-	// Redundant Gamepad Buttons (note: G is for Gamepad)
-	// Hanging Mechanism
-	public static JoystickButton coilG = new JoystickButton(leftJoystick, 1);
-	public static JoystickButton uncoilG = new JoystickButton(rightJoystick, 1);
-	// Intake
-	public static JoystickButton intakeG = new JoystickButton(leftJoystick, 2);
-	public static JoystickButton outtakeG = new JoystickButton(rightJoystick, 2);
-	// Shooter
-	public static JoystickButton shootG = new JoystickButton(leftJoystick, 4);
-	// Hopper
-	public static JoystickButton dispenseG = new JoystickButton(leftJoystick, 3);
-	// Ball Feed
-	public static JoystickButton feedG = new JoystickButton(rightJoystick, 4);
-	// Drivetrain Gear Shifting
-	public static JoystickButton shift = new JoystickButton(rightJoystick, 3);
+	public static JoystickButton feedG = new JoystickButton(gamepad, 1);
 
 	public OI() {
 		/*
@@ -69,8 +53,8 @@ public class OI {
 		coilG.whileHeld(new Hang(1));
 		uncoilG.whileHeld(new Hang(-1));
 		// Intake
-		intakeG.whileHeld(new Intake(1));
-		outtakeG.whileHeld(new Intake(-1));
+		intakeG.whileHeld(new Intake());
+		outtakeG.whileHeld(new Intake());
 		// Shooter
 		shootG.toggleWhenPressed(new Shoot());
 		// Hopper
