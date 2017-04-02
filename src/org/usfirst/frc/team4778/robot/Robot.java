a5deec0d590bb1201acf0d7a7a1a4936af23aaf5
ethanejohnsons <ethan@ejohnsons.com>
2017-04-02 13:50:34 -0500

    Added some spaghetti

--- src/org/usfirst/frc/team4778/robot/Robot.java
+++ src/org/usfirst/frc/team4778/robot/Robot.java
@@ -7,6 +7,9 @@ import org.usfirst.frc.team4778.robot.subsystems.Hopper;
 import org.usfirst.frc.team4778.robot.subsystems.Intake;
 import org.usfirst.frc.team4778.robot.subsystems.Shooter;
 
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
@@ -26,6 +29,8 @@ public class Robot extends IterativeRobot {
 	public Solenoid gearChuteOut = RobotMap.gearChuteOut;
 	public static OI oi = new OI();
 	Command autonomousCommand;
+	StringBuilder _sb = new StringBuilder();
+	int _loops = 0;
 
 	@Override
 	public void robotInit() {
@@ -35,6 +40,20 @@ public class Robot extends IterativeRobot {
 		drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
 		gearChuteIn.set(true);
 		gearChuteIn.set(false);
+		/* first choose the sensor */
+		RobotMap.S0.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		RobotMap.S0.reverseSensor(false);
+		//_talon.configEncoderCodesPerRev(XXX), // if using FeedbackDevice.QuadEncoder
+		//_talon.configPotentiometerTurns(XXX), // if using FeedbackDevice.AnalogEncoder or AnalogPot
+		/* set the peak and nominal outputs, 12V means full */
+		RobotMap.S0.configNominalOutputVoltage(+0.0f, -0.0f);
+		RobotMap.S0.configPeakOutputVoltage(+12.0f, -12.0f);
+		/* set closed loop gains in slot0 */
+		RobotMap.S0.setProfile(0);
+		RobotMap.S0.setF(0.1097);
+		RobotMap.S0.setP(0.22);
+		RobotMap.S0.setI(0);
+		RobotMap.S0.setD(0);
 	}
 
 	@Override
@@ -60,9 +79,36 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void disabledInit() {}
 
+	// SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI SPAGHETTI
 	@Override
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
+		double motorOutput = RobotMap.S0.getOutputVoltage() / RobotMap.S0.getBusVoltage();
+		/* prepare line to print */
+		_sb.append("\tout:");
+		_sb.append(motorOutput);
+		_sb.append("\tspd:");
+		_sb.append(RobotMap.S0.getSpeed());
+		if (OI.gamepad.getRawButton(2)) {
+			/* Speed mode */
+			double targetSpeed = 1160.25; //leftYstick * 1500.0; /* 1500 RPM in either direction */
+			RobotMap.S0.changeControlMode(TalonControlMode.Speed);
+			RobotMap.S0.set(targetSpeed); /* 1500 RPM in either direction */
+			/* append more signals to print when in speed mode. */
+			_sb.append("\terr:");
+			_sb.append(RobotMap.S0.getClosedLoopError());
+			_sb.append("\ttrg:");
+			_sb.append(targetSpeed);
+		} else {
+			/* Percent voltage mode */
+			RobotMap.S0.changeControlMode(TalonControlMode.PercentVbus);
+			RobotMap.S0.set(0);
+		}
+		if (++_loops >= 10) {
+			_loops = 0;
+			System.out.println(_sb.toString());
+		}
+		_sb.setLength(0);
 	}
 
 	@Override
