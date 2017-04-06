522c54308743879a38aacef4d822288f5c9b51f8
ethanejohnsons <ethan@ejohnsons.com>
2017-04-06 09:25:01 -0500

    Updated Center Gear Auto

--- src/org/usfirst/frc/team4778/robot/commands/auto/AutoCenterGear.java
+++ src/org/usfirst/frc/team4778/robot/commands/auto/AutoCenterGear.java
@@ -7,7 +7,7 @@ import org.usfirst.frc.team4778.robot.commands.Delay;
 public class AutoCenterGear extends Auto {
 	@Override
 	public void autoRun() {
-		addSequential(new AutoDrive(1));
+		addSequential(new AutoDrive(2));
 		addSequential(new Delay(0.5));
 		addSequential(new Chute());
 		addSequential(new Delay(1));
